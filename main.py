203a5d9608bf2d90ef53d4596f0c6755a51a0c92
LizH1008 <71219422+LizH1008@users.noreply.github.com>
2023-12-05 14:35:21 -0500

    camera

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ mp_face_mesh = mp.solutions.face_mesh  # initialize the face mesh model
 
 
 # camera stream:
-cap = cv2.VideoCapture(0)  # chose camera index (try 1, 2, 3)
+cap = cv2.VideoCapture(1)  # chose camera index (try 1, 2, 3)
 with mp_face_mesh.FaceMesh(
         max_num_faces=1,  # number of faces to track in each frame
         refine_landmarks=True,  # includes iris landmarks in the face mesh model
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as face_mesh:
     while cap.isOpened():
-        success, image = cap.read(0)
+        success, image = cap.read()
         if not success:  # no frame input
             print("Ignoring empty camera frame.")
             continue
